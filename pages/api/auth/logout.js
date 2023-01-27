2e9c59d8329dce75de42aeef0179007b283f09c5
Phonedolly <c1john0803@gmail.com>
2023-01-27 20:28:47 +0000

    port publish(not work)

--- pages/api/auth/logout.js
+++ pages/api/auth/logout.js
@@ -2,8 +2,7 @@ import redisClient from "@/lib/redis";
 import { deleteCookie } from "cookies-next";
 
 export default async function handler(req, res) {
-  console.log(`hello `, req.headers.authorization);
-  await redisClient;
+  await redisClient();
   await redisClient.setEx(req.headers.authorization.split(" ")[1],
     process.env.ACCESS_TOKEN_BLACKLIST_EXPIRE_TIME, 'logout')
   deleteCookie('refreshToken', { req, res });
