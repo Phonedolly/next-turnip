6c617083fe190eaa5d6aee712f0f4d407e72142c
Phonedolly <c1john0803@gmail.com>
2023-02-01 17:29:55 +0000

    apply google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,10 @@ import { QueryClient, QueryClientProvider } from 'react-query'
 
 import '@/styles/globals.css'
 
+import Script from 'next/script'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
+
 import localFont from '@next/font/local'
 
 const spoqa = localFont({
@@ -21,12 +25,47 @@ const spoqa = localFont({
 
 const queryClient = new QueryClient();
 
+
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <main className={spoqa.className}>
-        <Component {...pageProps} />
-      </main>
-    </QueryClientProvider>
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <QueryClientProvider client={queryClient}>
+        <main className={spoqa.className}>
+          <Component {...pageProps} />
+        </main>
+      </QueryClientProvider>
+    </>
   )
 }
